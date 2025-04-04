ade595c04065ac41bc71a64406eeee197317f598
zaki709 <t0709zaki@gmail.com>
2025-04-04 14:27:44 +0900

    add hlchunk

---
+++ lua/plugins/hlchunk.lua
@@ -0,0 +1,18 @@
+return {
+  "shellRaining/hlchunk.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("hlchunk").setup({
+      chunk = {
+        chars = {
+          horizontal_line = "─",
+          vertical_line = "│",
+          left_top = "╭",
+          left_bottom = "╰",
+          right_arrow = ">",
+        },
+        style = "#806d9c",
+      },
+    })
+  end,
+}
