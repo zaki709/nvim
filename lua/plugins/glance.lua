6660994bd89914ca04d190fd7cf50ab88458c532
zaki709 <t0709zaki@gmail.com>
2025-04-16 22:57:13 +0900

    add plugin glance.lua

---
+++ lua/plugins/glance.lua
@@ -0,0 +1,9 @@
+return {
+  "dnlhc/glance.nvim",
+  cmd = "Glance",
+  keys = {
+    { "<leader>gld", "<cmd>Glance definitions<cr>", desc = "Glance definitions" },
+    { "<leader>glr", "<cmd>Glance references<cr>", desc = "Glance references" },
+    { "<leader>glt", "<cmd>Glance type_definitions<cr>", desc = "Glance Type Definitions" },
+  },
+}
