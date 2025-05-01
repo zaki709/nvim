7c892a7444155b7f57b9a0939a26c85741ad93fb
zaki709 <t0709zaki@gmail.com>
2025-05-02 00:46:58 +0900

    enable avante

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,17 @@
+# copilot.lua
 return {
-  "github/copilot.vim",
-  lazy = false,
+ "zbirenbaum/copilot.lua",
+ cmd = "Copilot", -- :Copilotコマンドが実行されたときにプラグインを読み込む
+ build = ":Copilot auth", -- プラグインロード後左記のコマンドを実行
+ event = "BufReadPost", -- ファイルが読み込まれた後にプラグインをロード. https://vim-jp.org/vimdoc-ja/autocmd.html
+ config = function()
+  require("copilot").setup({
+   suggestion = {
+    auto_trigger = true,
+    keymap = {
+     accept = "<Tab>", -- Tab で提案を受け入れる
+    },
+   },
+  })
+ end,
 }
