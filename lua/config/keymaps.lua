e3592293e763cff86363c05c98014fcd0612f33a
zaki709 <t0709zaki@gmail.com>
2025-06-20 18:24:44 +0900

    remove keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,3 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
